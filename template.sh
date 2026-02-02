8272b540b7afae41d0b78b3453fb65c97e5fae69
Aditya Pandey <aditya12bone@gmail.com>
2026-02-03 00:31:25 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,20 @@
+mkdir -p src
+mkdir -p research
+
+touch src/_init_.py
+
+touch src/helper.py
+
+touch src prompt.py
+
+touch .env
+
+touch setup.py
+
+touch app.py
+
+touch research/trials.ipynb
+
+touch requirements.txt
+
+echo "directory and file created"
